1d68c9646ae977beef555fe8be02a3aeac31c8b1
John Strack <John.Strack@LoopLoc.com>
2013-02-11 16:54:19 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml
    
    	Chapter 4: Variables and Objects (with Time)

--- studio_game.rb
+++ studio_game.rb
@@ -1,3 +1,5 @@
+startTime = Time.new()
+
 player1 = 'larry'
 player2 = 'curly'
 player3 = 'moe'
@@ -13,4 +15,5 @@ puts "#{player2.upcase} has a health of #{health2}."
 puts "#{player3.capitalize} has a health of #{health3}.".center(50, '*')
 puts "#{player4.capitalize.ljust(30, '.')} #{health4} health."
 
+puts "#{startTime.strftime("The game started on %A %m/%d/%Y at %l:%M%p")}"
 #puts "Players:\n\t#{player1}\n\t#{player2}\n\t#{player3}"
