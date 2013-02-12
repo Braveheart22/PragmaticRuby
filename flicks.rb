3d73e7e4b7b27642e03dff176fbcb2de8e1090d9
John Strack <John.Strack@LoopLoc.com>
2013-02-12 16:47:24 -0500

    Merge remote-tracking branch 'origin/master' 	Chapter 7: Classes

--- flicks.rb
+++ flicks.rb
@@ -1,19 +1,8 @@
-title1 = "goonies"
-rank1 = 10
-puts "#{title1.capitalize} has a rank of #{rank1}"
-
-title2 = "ghostbusters"
-rank2 = 9
-puts "#{title2.capitalize} has a rank of #{rank2}"
-
-title3 = "goldfinger"
-rank3 = 8
-puts "#{title3.capitalize} has a rank of #{rank3}"
-
 def weekday
   current_time = Time.new
   current_time.strftime("%A")
 end
+
 def movie_listing (title, rank=0)
   "#{title.capitalize} has a rank of #{rank} as of #{weekday}."
 end
