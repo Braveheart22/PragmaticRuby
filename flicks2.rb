9e80297246e616bd92bc8b8f271a1301b211834c
John Strack <John.Strack@LoopLoc.com>
2013-02-15 14:02:09 -0500

    Merge remote-tracking branch 'origin/master' 	Chapter 11: Separate Source Files (Movies)

--- flicks2.rb
+++ flicks2.rb
@@ -1,46 +1,6 @@
-class Movie
-  attr_reader :rank
-  attr_accessor :title
-  def initialize(title, rank=0)
-    @title = title.capitalize
-    @rank = rank
-  end
+require_relative 'movie'
+require_relative 'playlist'
 
-  def to_s
-    "#{@title} has a rank of #{@rank}"
-  end
-
-  def thumbs_up
-    @rank += 1
-  end
-
-
-  def thumbs_down
-    @rank -= 1
-  end
-end
-
-class Playlist
-  def initialize(name)
-    @name = name
-    @movies = []
-  end
-
-  def add_movie(movie)
-    @movies << movie
-  end
-
-  def play
-    puts "#{@name}'s playlist:"
-    puts @movies
-
-    @movies.each do |movie|
-      movie.thumbs_up
-      puts movie
-    end
-
-  end
-end
 movie1 = Movie.new("goonies", 10)
 movie2 = Movie.new("ghostbusters", 9)
 movie3 = Movie.new("goldfinger")
