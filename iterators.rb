0bfc600637ebd38d2ac4f7a1e384058c1223e208
John Strack <John.Strack@LoopLoc.com>
2013-03-04 17:20:47 -0500

    Unit 16: Symbols and Structs (Exercise)

--- iterators.rb
+++ iterators.rb
@@ -1,10 +1,30 @@
-def thrice
-  puts "Ready."
-  yield(1)
-  puts "Set"
-  yield(2)
-  puts "Go."
-  yield (3)
+#def thrice
+#  puts "Ready."
+#  yield(1)
+#  puts "Set"
+#  yield(2)
+#  puts "Go."
+#  yield (3)
+#end
+#
+#thrice {|number| puts "Running your block.(#{number})"}
+
+#def conversation
+#  puts "Hello"
+#  yield
+#  puts"Goodbye"
+#end
+#
+#conversation {puts "Good to meet you!"}
+
+def n_times(count)
+  1.upto(count) do |n|
+    yield n
+  end
 end
 
-thrice {|number| puts "Running your block.(#{number})"}
+n_times(15) do |n|
+  puts "#{n} situps"
+  puts "#{n} pushups"
+  puts "#{n} chinups"
+end
