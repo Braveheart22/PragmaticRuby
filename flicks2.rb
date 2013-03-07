5f14c9672e7858ca7be3eb6ac917a21410f63f0d
John Strack <John.Strack@LoopLoc.com>
2013-03-07 16:57:42 -0500

    Unit 19: Input/Output (Video - start)

--- flicks2.rb
+++ flicks2.rb
@@ -5,12 +5,22 @@ movie1 = Movie.new("goonies", 10)
 movie2 = Movie.new("ghostbusters", 9)
 movie3 = Movie.new("goldfinger")
 
-#movies = [movie1, movie2, movie3]
-
 playlist1 = Playlist.new("Kermit")
 playlist1.add_movie(movie1)
 playlist1.add_movie(movie2)
 playlist1.add_movie(movie3)
-playlist1.play(3)
 
-playlist1.print_stats
+loop do
+  puts "\nHow many viewings? ('quit' to exit)"
+  answer = gets.chomp.downcase
+  case answer
+    when /^\d+$/
+      playlist1.play(answer.to_i)
+    when 'quit', 'exit'
+      playlist1.print_stats
+      break
+    else
+      puts "Please enter a number or 'quit'."
+  end
+end
+
