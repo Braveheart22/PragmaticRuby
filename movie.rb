221d27140154a95d9e2bd338bc03e7bff5f12361
John Strack <John.Strack@LoopLoc.com>
2013-03-14 17:01:17 -0400

    Unit 21: Mixins (Videos)

--- movie.rb
+++ movie.rb
@@ -1,6 +1,11 @@
+require_relative 'rankable'
+
 class Movie
-  attr_reader :rank
+  include Rankable
+
+  attr_accessor :rank
   attr_accessor :title
+
   def initialize(title, rank=0)
     @title = title.capitalize
     @rank = rank
@@ -33,29 +38,10 @@ class Movie
     @snack_carbs.values.reduce(0, :+)
   end
 
-  def <=>(other_movie)
-    other_movie.rank <=> @rank
-  end
-
-  def hit?
-    @rank >= 10
-  end
-
-  def status
-    hit? ? "Hit" : "Flop"
-  end
-
   def to_s
     "#{@title} has a rank of #{@rank} (#{status})"
   end
 
-  def thumbs_up
-    @rank += 1
-  end
-
-  def thumbs_down
-    @rank -= 1
-  end
 end
 
 if __FILE__ == $0
